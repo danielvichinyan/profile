fa95cbf21738ae7281326d68d28d9626c70c8747
danielvichinyan <vichinyan.daniel@gmail.com>
2021-04-07 14:24:42 +0300

    Implemented Authorization header filter in profile security

--- src/main/java/com/knowit/profile/security/filters/AuthorizationHeaderFilter.java
+++ src/main/java/com/knowit/profile/security/filters/AuthorizationHeaderFilter.java
@@ -1,5 +1,10 @@
 package com.knowit.profile.security.filters;
 
+import com.knowit.profile.domain.entities.User;
+import com.knowit.profile.exceptions.UserDoesNotExistException;
+import com.knowit.profile.services.ProfileService;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -8,12 +13,18 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.ArrayList;
 
 @Component
 public class AuthorizationHeaderFilter extends OncePerRequestFilter {
 
     private static final String X_USER_ID = "X-User-Id";
 
+    private final ProfileService profileService;
+
+    public AuthorizationHeaderFilter(ProfileService profileService) {
+        this.profileService = profileService;
+    }
 
     @Override
     protected void doFilterInternal(
@@ -21,6 +32,26 @@ public class AuthorizationHeaderFilter extends OncePerRequestFilter {
             HttpServletResponse httpServletResponse,
             FilterChain filterChain
     ) throws ServletException, IOException {
+        String userId = httpServletRequest.getHeader(X_USER_ID);
 
+        if (userId == null) {
+            httpServletResponse.setStatus(403);
+            throw new ServletException("Invalid user id!");
+        }
+
+        try {
+            User user = this.profileService.fetchUserById(userId);
+            UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+                    user,
+                    null,
+                    new ArrayList<>()
+            );
+
+            SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+        } catch (UserDoesNotExistException e) {
+            logger.error(e.getStackTrace(), e);
+        }
+
+        filterChain.doFilter(httpServletRequest, httpServletResponse);
     }
 }
